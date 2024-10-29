2d8ce9e1514747bb9d397226dd61d516b113f0d4
alvarezleiva <DAM1@aula11-pc06>
2024-10-29 09:10:57 +0100

    cambios en hola y adios

--- Reto0/src/retos/Reto0.java
+++ Reto0/src/retos/Reto0.java
@@ -3,5 +3,8 @@ package retos;
 public class Reto0 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo222");
+		
+		System.out.println("Hola y adios");
+		System.out.println("adiosssssssssss");
 	}
 }
